477325788dc58a7e23ccab8c5911aa1bdbed5e7c
Benjamin Shuster <bentzishuster613@gmail.com>
2023-08-04 15:14:09 -0400

    add text to about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+At Cats and Coffee, we believe in the power of delightful moments, where the warmth of a perfectly brewed cup of coffee harmoniously blends with the charming presence of feline friends. Our unique establishment is a haven for cat enthusiasts and coffee aficionados alike, where the soothing purrs of cats complement the rich aroma of freshly roasted coffee beans.
+
+Our Story
+The tale of Cats and Coffee began with a simple yet profound idea: to create a space that celebrates the joys of life through the companionship of cats and the comfort of coffee. Founded by a group of passionate cat lovers and dedicated coffee connoisseurs, our journey started with a dream of fostering a welcoming environment where people could escape the bustle of daily life and find solace in the company of our four-legged friends.
+
+    </div>
   )
 }
 
