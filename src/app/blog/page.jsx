a28ae85790d1ca4625ffedf13357fea481dc9681
Benjamin Shuster <bentzishuster613@gmail.com>
2023-08-06 21:42:31 -0400

    add blog

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,36 @@
+import { getAllPostsData } from '@/lib/posts'
+import Link from 'next/link'
 import React from 'react'
-
+import styles from "./blog.module.css";
+import {Inknut_Antiqua, Open_Sans} from 'next/font/google'
+const inknut = Inknut_Antiqua({display:"swap",subsets:["latin"],weight:["500"]})
+const opensans = Open_Sans({display:"swap",subsets:["latin"],weight:["500","700"]})
+let data = getAllPostsData()
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <>
+    <div className={styles.container}>
+
+
+    {data.map((post)=>{
+      return(
+        //id, title, date
+        <div className={styles.post}>
+        <Link href={`/blog/${post.id}`} className={styles.link}>
+          <div className={styles.top}>
+            <p className={inknut.className}>{ post.title }</p>
+            <p className={opensans.className}> {post.date}</p>
+          </div>
+      <p className={[styles.shortdescription,opensans.className].join(" ")}>
+        {post.shortdescription}
+        </p>
+        </Link>
+          </div>
+      )
+    })
+    }
+    </div>
+    </>
   )
 }
 
