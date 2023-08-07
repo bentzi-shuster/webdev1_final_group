f0be63229001da722e607aa2562277e01834cdd8
Stitchiiee <is328@njit.edu>
2023-08-06 22:06:03 -0400

    aboutpage

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,13 +1,61 @@
+
 import React from 'react'
+import styles from "./about.module.css"
+import { Inknut_Antiqua } from 'next/font/google'
+
 
 const About = () => {
   return (
-    <div>
+    <div className = "aboutdiv"> 
+      <h1>About Cats and Coffee</h1> 
+    
+
 At Cats and Coffee, we believe in the power of delightful moments, where the warmth of a perfectly brewed cup of coffee harmoniously blends with the charming presence of feline friends. Our unique establishment is a haven for cat enthusiasts and coffee aficionados alike, where the soothing purrs of cats complement the rich aroma of freshly roasted coffee beans.
 
-Our Story
+<h3>Our Story</h3>
+
 The tale of Cats and Coffee began with a simple yet profound idea: to create a space that celebrates the joys of life through the companionship of cats and the comfort of coffee. Founded by a group of passionate cat lovers and dedicated coffee connoisseurs, our journey started with a dream of fostering a welcoming environment where people could escape the bustle of daily life and find solace in the company of our four-legged friends.
 
+<h3>The Cats</h3>
+Our resident feline family is the heart and soul of Cats and Coffee. Each cat has its own unique personality, from the curious explorers to the gentle cuddlers. We've carefully curated a haven for our furry friends, complete with cozy nooks, climbing spaces, and sunny spots perfect for catnaps. Our cats are not just pets; they are an integral part of our community, bringing smiles and a sense of calm to all who visit.
+
+<h3>The Coffee</h3>
+
+Just as we cherish our cats, we are equally passionate about serving exceptional coffee. Our baristas are skilled artisans, meticulously crafting each cup to perfection. We source our beans from sustainable and ethical farms around the world, ensuring that every sip tells a story of dedication and quality. Whether you're a fan of a classic cappuccino or an adventurous matcha latte, our diverse menu caters to all tastes.
+
+<h3>A Meeting Place</h3>
+
+Cats and Coffee is more than a cafe; it's a meeting place for kindred spirits to connect, unwind, and create memories. Whether you're seeking a moment of solitude with a book and a purring companion, engaging in heartfelt conversations with fellow cat lovers, or simply seeking respite from a busy day, our space welcomes you with open arms.
+
+<h3>Community and Connection</h3>
+
+We are committed to nurturing a strong sense of community. Cats and Coffee hosts events that bring people together, from cat-themed art exhibitions to coffee tasting workshops. We also partner with local animal shelters, contributing to the well-being of cats beyond our walls. Our dedication to connection extends to the digital realm, where our online platform serves as a hub for cat care tips, coffee insights, and heartwarming stories.
+
+<h3>Visit Us</h3>
+
+Come join us at Cats and Coffee, where the enchanting world of cats intertwines with the comforting embrace of coffee. Whether you're seeking a moment of tranquility, the thrill of discovery, or simply a heartwarming experience, our doors are open, and our cats are ready to welcome you with their playful spirits and soothing presence.
+
+Indulge in the harmony of cats and coffee, and let your senses embark on a journey of delight and wonder.
+
+<h3>Address:</h3>
+[Insert Address]
+
+<h3>Hours:</h3>
+Monday - Saturday: 9:00 AM - 7:00 PM
+Sunday: 10:00 AM - 5:00 PM
+
+<h3>Contact:</h3>
+Phone: [973-385-5438]
+<br></br>
+Email: [Catsandcoffee@gmail.com]
+
+Follow us on social media for the latest updates and heartwarming moments:
+<br></br>
+Facebook: [https://www.facebook.com/]
+<br></br>
+Instagram: [https://www.instagram.com/]
+<br></br>
+Twitter: [https://twitter.com/]
     </div>
   )
 }
