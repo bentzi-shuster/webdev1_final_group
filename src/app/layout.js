cb64c78651180f9e5172e45a50eff0f1ca4b943a
nz68 <nz68@njit.edu>
2023-08-09 09:40:23 -0400

    google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={""}>
+        {/* <!-- Google tag (gtag.js) --> */}
+<Script strategy='afterInteractive' src="https://www.googletagmanager.com/gtag/js?id=G-TJTHF7S3Q7"></Script>
+<Script strategy='afterInteractive' dangerouslySetInnerHTML={{__html:`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TJTHF7S3Q7');
+
+`}} id="gtag"></Script>
         <Nav />
         <div style={{minHeight: "60vh"}}>
         {children}          
